67d9cbdeab9a66fd84847d630afbe6fb8128a496
Shleo <leohlim@gmail.com>
2019-02-11 18:55:58 -0600

    Activate win when spongebob is found

---
+++ Yelp Maze Game/Assets/Scripts/WinCondition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinCondition : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            winGame();
+        }
+
+    }
+
+    // Update is called once per frame
+    void winGame()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
