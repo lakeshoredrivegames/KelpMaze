49b1cacc58045f0fdc07173fb157c3ab8bf58a31
wbondie <bondie.wes@gmail.com>
2019-02-14 17:34:49 -0600

    breakables shake and conch has global cooldown

--- Yelp Maze Game/Assets/Scripts/Gameplay/Items/Breakable.cs
+++ Yelp Maze Game/Assets/Scripts/Gameplay/Items/Breakable.cs
@@ -10,6 +10,7 @@ namespace KelpMaze.Gameplay
         private void Start()
         {
             audio = this.gameObject.AddComponent<AudioSource>();
+            originalPos = this.gameObject.transform.localPosition;
         }
 
         /* Got hit by something */
@@ -20,9 +21,29 @@ namespace KelpMaze.Gameplay
                 audio.clip = attackedClip;
                 audio.Play();
                 Health -= player.swordLevel;
+                isShaking = true;
                 if (Health <= 0)
                     Broken(player);
             }
+        }
+
+        public void Update()
+        {
+            if(isShaking)
+            {
+                if(shakeDuration > 0)
+                {
+                    this.gameObject.transform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+                    shakeDuration -= Time.deltaTime * decreaseFactor;
+                }
+                else
+                {
+                    shakeDuration = 0.1f;
+                    this.gameObject.transform.localPosition = originalPos;
+                    isShaking = false;
+                }
+            }
+
         }
 
         public void Broken(PlayerManager player)
@@ -37,5 +58,11 @@ namespace KelpMaze.Gameplay
         public AudioClip attackedClip;
         public AudioClip brokenClip;
         public AudioSource audio;
+
+        private Vector3 originalPos;
+        private float shakeDuration = 0.1f;
+        private float shakeAmount = 0.1f;
+        private float decreaseFactor = 1.0f;
+        public bool isShaking;
     }
 }
--- Yelp Maze Game/Assets/Scripts/Gameplay/Items/ConchItem.cs
+++ Yelp Maze Game/Assets/Scripts/Gameplay/Items/ConchItem.cs
@@ -13,31 +13,48 @@ namespace KelpMaze.Gameplay
         }
         public override void Execute(PlayerManager player)
         {
-            if(isCooling)
+            if(!isGlobalCooling)
             {
-                noClips.Add(player.useConchClipNo);
-                noClips.Add(player.useConchClipMaybe);
-                int r = Random.Range(0, 2);
-                player.audio.clip = noClips[r];
-                player.audio.Play();
-                if(Time.time >= conchCooldownTime)
+                if(isCooling)
                 {
-                    isCooling = false;
+                    noClips.Add(player.useConchClipNo);
+                    noClips.Add(player.useConchClipMaybe);
+                    int r = Random.Range(0, 2);
+                    player.audio.clip = noClips[r];
+                    player.audio.Play();
+                    if(Time.time >= conchCooldownTime)
+                    {
+                        isCooling = false;
+                    }
                 }
+                else
+                {
+                    Debug.Log("Playing the conch!");
+                    player.audio.clip = player.useConchClipYes;
+                    player.audio.Play();
+                    isCooling = true;
+                    conchCooldownTime = Time.time + conchCooldown;
+                }
+
+                isGlobalCooling = true;
+                globalCooldownTime = Time.time + globalConchCooldown;
             }
             else
             {
-                Debug.Log("Playing the conch!");
-                player.audio.clip = player.useConchClipYes;
-                player.audio.Play();
-                isCooling = true;
-                conchCooldownTime = Time.time + conchCooldown;
+                if (Time.time >= globalCooldownTime)
+                {
+                    isGlobalCooling = false;
+                }
             }
         }
 
         private List<AudioClip> noClips;
-        private float conchCooldown = 1.5f;
+        private float conchCooldown = 5f;
         private bool isCooling;
         private float conchCooldownTime;
+
+        private float globalCooldownTime;
+        private float globalConchCooldown = 3f;
+        private bool isGlobalCooling;
     }
 }
