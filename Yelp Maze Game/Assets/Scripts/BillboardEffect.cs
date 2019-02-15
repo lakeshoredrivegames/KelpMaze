cc4be5d3aedc736ef1bf776a4a197ccb704783a7
Shleo <leohlim@gmail.com>
2019-02-14 22:03:22 -0600

    Tutorial text and campfirelighting

---
+++ Yelp Maze Game/Assets/Scripts/BillboardEffect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardEffect : MonoBehaviour
+{
+    private Camera my_camera;
+    void Start()
+    {
+        my_camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + my_camera.transform.rotation * Vector3.forward, my_camera.transform.rotation * Vector3.up);
+    }
+}
---
+++ Yelp Maze Game/Assets/Scripts/TextActivate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextActivate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private TextMesh textMesh;
+    void Start()
+    {
+        textMesh = this.gameObject.GetComponent<TextMesh>();
+        textMesh.gameObject.SetActive(false);
+
+    }
+    public void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag("Player"))        
+        textMesh.gameObject.SetActive(true);
+    }
+
+    public void OnTriggerExit(Collider other) 
+    {
+        if (other.gameObject.CompareTag("Player"))        
+        textMesh.gameObject.SetActive(false);
+    }
+}
