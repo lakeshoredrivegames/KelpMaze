fad2cb5c7ab47808c82f080d54db724634df4d5d
wbondie <bondie.wes@gmail.com>
2019-02-17 14:46:43 -0600

    Added mouse cursor support

---
+++ Kelp Maze Game/Assets/Scripts/ShowMouse.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KelpMaze.Gameplay
+{
+    public class ShowMouse : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+    }
+}
