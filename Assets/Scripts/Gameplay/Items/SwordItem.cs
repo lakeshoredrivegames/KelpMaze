51a5a6c3a845414685792f40b23a92672c5adcde
leohlim <LEOHLIM@GMAIL.COM>
2019-11-24 23:10:31 -0600

    Reverted

--- Assets/Scripts/Gameplay/Items/SwordItem.cs
+++ Assets/Scripts/Gameplay/Items/SwordItem.cs
@@ -6,12 +6,6 @@ namespace KelpMaze.Gameplay
 {
     public class SwordItem : Equipable
     {
-        private float maxAttackRange = 1.5f;
-        private float swordCooldown = 0.5f;
-        private bool isCooling;
-        private float swordCooldownTime;
-        private ParticleSystem playerParticles;
-        
         public override void Execute(PlayerManager player)
         {
             if (isCooling)
@@ -70,6 +64,10 @@ namespace KelpMaze.Gameplay
             }
         }
 
-        
+        private float maxAttackRange = 1f;
+        private float swordCooldown = 0.5f;
+        private bool isCooling;
+        private float swordCooldownTime;
+        private ParticleSystem playerParticles;
     }
 }
